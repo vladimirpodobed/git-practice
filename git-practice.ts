95e81e1dee27867cca471f1fad2b192f2f8906b1
podobedv <podobedv@gmail.com>
2026-09-22 15:26:22 +0200

    Initial commit

---
+++ git-practice.ts
@@ -0,0 +1,63 @@
+//GIT-practice:
+//
+//STEP 1 — Create a new project
+//WebStorm: File → New → Project/Name
+
+//STEP 2 — Check Git installation
+//Terminal command = git --version
+
+//STEP 3 — Initialize Git
+//Terminal command = git init
+
+//STEP 4 — Check Git status
+//Terminal command = git status
+
+//STEP 5 — Stage project files
+//Terminal command = git add .  (Точка . означает: добавить все файлы текущего проекта)
+
+//STEP 6 — Make the first commit:
+//Terminal command = git commit -m "Initial commit"
+
+//STEP 7 — Check commit history:
+//Terminal command = git log --oneline
+
+//STEP 8 — Create a GitHub repository
+//
+
+//STEP 9 — Connect the local project to the GitHub repository
+//
+
+//STEP 10 — Push the project to GitHub
+//
+
+//STEP 11 — Make some changes
+//
+
+//STEP 12 — Check the changes: Terminal = git status
+//
+
+//STEP 13 — Create a second commit
+//
+
+//STEP 14 — Push the changes to GitHub
+//
+
+//STEP 15 — Create a new branch
+//
+
+//STEP 16 — Make changes on the new branch
+//
+
+//STEP 17 — Create a commit on the new branch
+//
+
+//STEP 18 — Push the branch to GitHub
+//
+
+//STEP 19 — Open a Pull Request
+//
+
+//STEP 20 — Merge the Pull Request into the main branch
+//
+
+//STEP 21 — Check the final result
